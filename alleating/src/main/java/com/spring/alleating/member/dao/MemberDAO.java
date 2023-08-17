d0845c80b6dd824239fea4109662c861039c4bf2
Administrator <Administrator@User -2023RGUJE>
2023-08-17 21:10:35 +0900

    memberVO및 member mapper파일 작성중

--- alleating/src/main/java/com/spring/alleating/member/dao/MemberDAO.java
+++ alleating/src/main/java/com/spring/alleating/member/dao/MemberDAO.java
@@ -1,5 +1,5 @@
 package com.spring.alleating.member.dao;
 
-public class memberDAOImpl {
+public interface MemberDAO {
 
 }
--- alleating/src/main/java/com/spring/alleating/member/vo/MemberVO.java
+++ alleating/src/main/java/com/spring/alleating/member/vo/MemberVO.java
@@ -1,5 +1,205 @@
 package com.spring.alleating.member.vo;
 
-public class MemberVO {
+import java.sql.Date;
 
+import org.springframework.stereotype.Component;
+
+@Component("memberVO")
+public class MemberVO {
+    private String id;
+    private String pwd;
+    private String join_type;
+    private String name;
+    private String email;
+    private String hp1;
+    private String hp2;
+    private String hp3;
+    private String zipcode;
+    private String address;
+    private String address2;
+    private String address_detail;
+    private String gender;
+    private String birth_year;
+    private String birth_month;
+    private String birth_day;
+    private String sms_yn;
+    private String email_yn;
+    private String member_grade;
+    private String del_yn;
+    private String owner_name;
+    private String owner_num;
+    private String owner_tel1;
+    private String owner_tel2;
+    private String owner_tel3;
+    private Date joinDate;
+    private String personalInformation;
+    
+    public MemberVO() {
+    	
+    }
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getJoin_type() {
+		return join_type;
+	}
+	public void setJoin_type(String join_type) {
+		this.join_type = join_type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHp1() {
+		return hp1;
+	}
+	public void setHp1(String hp1) {
+		this.hp1 = hp1;
+	}
+	public String getHp2() {
+		return hp2;
+	}
+	public void setHp2(String hp2) {
+		this.hp2 = hp2;
+	}
+	public String getHp3() {
+		return hp3;
+	}
+	public void setHp3(String hp3) {
+		this.hp3 = hp3;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getAddress_detail() {
+		return address_detail;
+	}
+	public void setAddress_detail(String address_detail) {
+		this.address_detail = address_detail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth_year() {
+		return birth_year;
+	}
+	public void setBirth_year(String birth_year) {
+		this.birth_year = birth_year;
+	}
+	public String getBirth_month() {
+		return birth_month;
+	}
+	public void setBirth_month(String birth_month) {
+		this.birth_month = birth_month;
+	}
+	public String getBirth_day() {
+		return birth_day;
+	}
+	public void setBirth_day(String birth_day) {
+		this.birth_day = birth_day;
+	}
+	public String getSms_yn() {
+		return sms_yn;
+	}
+	public void setSms_yn(String sms_yn) {
+		this.sms_yn = sms_yn;
+	}
+	public String getEmail_yn() {
+		return email_yn;
+	}
+	public void setEmail_yn(String email_yn) {
+		this.email_yn = email_yn;
+	}
+	public String getMember_grade() {
+		return member_grade;
+	}
+	public void setMember_grade(String member_grade) {
+		this.member_grade = member_grade;
+	}
+	public String getDel_yn() {
+		return del_yn;
+	}
+	public void setDel_yn(String del_yn) {
+		this.del_yn = del_yn;
+	}
+	public String getOwner_name() {
+		return owner_name;
+	}
+	public void setOwner_name(String owner_name) {
+		this.owner_name = owner_name;
+	}
+	public String getOwner_num() {
+		return owner_num;
+	}
+	public void setOwner_num(String owner_num) {
+		this.owner_num = owner_num;
+	}
+	public String getOwner_tel1() {
+		return owner_tel1;
+	}
+	public void setOwner_tel1(String owner_tel1) {
+		this.owner_tel1 = owner_tel1;
+	}
+	public String getOwner_tel2() {
+		return owner_tel2;
+	}
+	public void setOwner_tel2(String owner_tel2) {
+		this.owner_tel2 = owner_tel2;
+	}
+	public String getOwner_tel3() {
+		return owner_tel3;
+	}
+	public void setOwner_tel3(String owner_tel3) {
+		this.owner_tel3 = owner_tel3;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public String getPersonalInformation() {
+		return personalInformation;
+	}
+	public void setPersonalInformation(String personalInformation) {
+		this.personalInformation = personalInformation;
+	}
+    
+    
 }
