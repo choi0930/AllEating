99864b7e916a82f600525481ca9ac0a0d5710143
Administrator <Administrator@User -2023RGUJE>
2023-08-23 21:08:48 +0900

    cart

--- alleating/src/main/java/com/spring/alleating/cart/controller/CartController.java
+++ alleating/src/main/java/com/spring/alleating/cart/controller/CartController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,7 +14,7 @@ import com.spring.alleating.cart.vo.CartVO;
 
 public interface CartController {
 	public ModelAndView myCartMain(HttpServletRequest request, HttpServletResponse response)  throws Exception;
-	public @ResponseBody String addProductInCart(@RequestParam("id") String id, @RequestParam("productId") int productId,@RequestParam("cart_product_qty") int cart_product_qty, HttpServletRequest request, HttpServletResponse response)  throws Exception;
-	public  @ResponseBody CartVO modifyCartQty(@RequestParam("cartId")int cartId,@RequestParam("productId") int productId,@RequestParam("cart_product_qty")int qty,@RequestParam("status") String status, HttpServletRequest request, HttpServletResponse response)  throws Exception;
+	public @ResponseBody String addProductInCart(@RequestParam("productId") String productId,@RequestParam("cart_product_qty")String select_qty, HttpServletRequest request, HttpServletResponse response)  throws Exception;
+	public  @ResponseBody String modifyCartQty(HttpServletRequest request, HttpServletResponse response)  throws Exception;
 	public ModelAndView removeCartProduct(@RequestParam("cartId") int cartId,HttpServletRequest request, HttpServletResponse response)  throws Exception;
 }
