a0a5a0f7b06c7be3852de66dbb04c1acfa6438e8
choi09 <0930alan@gmail.com>
2023-09-01 01:07:08 +0900

    commit

--- alleating/src/main/java/com/spring/alleating/admin/community/controller/AdminCommunityControllerImpl.java
+++ alleating/src/main/java/com/spring/alleating/admin/community/controller/AdminCommunityControllerImpl.java
@@ -1,5 +1,13 @@
 package com.spring.alleating.admin.community.controller;
 
-public class AdminCommunityControllerImpl {
+import org.springframework.web.servlet.ModelAndView;
 
+public class AdminCommunityControllerImpl {
+	
+	public ModelAndView apiTest()throws Exception{
+		ModelAndView mav = new ModelAndView();
+		
+		
+		return mav;
+	}
 }
